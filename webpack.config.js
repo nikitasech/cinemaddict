71c8dc3ec1506aab97af6b42fe45ef88be79d67e
NikitaSech <NikitaSech@yandex.ru>
2022-11-15 20:35:08 +0300

    Настраивает сборку
    - Добавляет Readme.md
    - Устанавливает зависимости
    - Настраивает webpack с babel
    - Настраивает .gitignore и .eslintignore
    - Подключает bandle.js к index.html

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const copyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bandle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new copyPlugin({
+      patterns: [{ from: 'public' }]
+    }),
+  ],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: ['babel-loader']
+    }]
+  },
+  performance: {
+    maxEntrypointSize: 1024000,
+    maxAssetSize: 1024000
+},
+}
